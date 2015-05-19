26a7300653a5c65e9ad47d7cca1cd0a97d9de9dc
flar2 <asegaert@gmail.com>
2015-05-18 21:35:32 -0400

    sched: make min_budget tunable
    Signed-off-by: flar2 <asegaert@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -30,12 +30,13 @@
 #include <linux/mempolicy.h>
 #include <linux/migrate.h>
 #include <linux/task_work.h>
+#include <linux/module.h>
 
 #include "sched.h"
 #include <trace/events/sched.h>
 
-
-#define MIN_BUDGET	40
+static int min_budget = 80;
+module_param(min_budget, int, 0755);
 
 unsigned int sysctl_sched_latency = 6000000ULL;
 unsigned int normalized_sysctl_sched_latency = 6000000ULL;
@@ -1978,7 +1979,7 @@ int sched_set_cpu_budget(int cpu, int budget)
 	struct rq *rq = cpu_rq(cpu);
 
 	if (cpu < 6)
-		rq->budget = max(budget, MIN_BUDGET);
+		rq->budget = max(budget, min_budget);
 	else
 		rq->budget = budget;
 
